03f7c3d437de7ae8550d77295dd5ccac135cbca5
Marcus Turnbo <marcus@emarcus.com>
2025-12-18 18:59:20 -0600

    Added functionality for scanning single wallet

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import json
 from app.models.portfolio import Portfolio
 import argparse
 
-def get_wallet_assets():
+def get_wallets():
     file_path = os.path.join('config', 'wallets.json')
     with open(file_path) as f:
         wallets = json.load(f)
+
+    return wallets
+
+
+def get_wallet_assets():
+    wallets = get_wallets()
     for wallet, tokens in wallets.items():
         portfolio = Portfolio(wallet, "wallet", tokens)
         portfolio.show_assets()
@@ -27,6 +33,15 @@ if __name__ == '__main__':
     parser.add_argument("--blockchain", type=str, required=False, help="Name of blockchain to scan")
     args = parser.parse_args()
 
+    wallets = get_wallets()
+
+    if args.wallet:
+        print(f"Scanning wallet: {args.wallet} ...")
+        tokens = wallets[args.wallet]
+        portfolio = Portfolio(args.wallet, "wallet", tokens)
+        portfolio.show_assets()
+
+
     # scan wallets and exchanges
-    # get_wallet_assets()
-    get_exchange_assets()
+    # get_wallet_assets(wallets)
+    # get_exchange_assets()
